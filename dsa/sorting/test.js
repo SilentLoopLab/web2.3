78e6aa6cdd478d80ffcabcabc6134902a27fe967
YeghyanAlen2004 <alenyeghyan74@gmail.com>
2026-05-24 12:39:03 +0400

    counting

--- dsa/sorting/test.js
+++ dsa/sorting/test.js
@@ -1,16 +1,15 @@
 import selectionSort from "./selection.js";
 import bubbleSort from "./bubble.js";
 import insertionSort from "./insertion.js";
-let arr1 = new Array(10000)
-let arr2 = new Array(10000)
-for (let i = 0; i < arr1.length; ++i) {
-    arr1[i] = Math.floor(((Math.random() * 100000)));
-}
-for (let i = 0; i < arr2.length; ++i) {
-    arr2[i] = i;
+import countingSort from "./counting.js";
+
+let arr = new Array(100000);
+
+for (let i = 0; i < 100000; ++i) {
+    arr[i] = Math.floor(Math.random() * 50000000 + 1);
 }
 
 let now = Date.now();
-bubble(arr1);
+let res = countingSort(arr);
 console.log(Date.now() - now);
-console.log(arr1);
+console.log(res);
